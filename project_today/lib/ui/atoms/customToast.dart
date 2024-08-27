b78e560e9f4500ba85672acbbe4963107c26ce4d
kweonsikyung <ekrk6632@naver.com>
2024-08-27 22:12:59 +0900

    feat: common custom toast

---
+++ project_today/lib/ui/atoms/customToast.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:project_today/core/constant/colors.dart';
+
+enum ToastType {
+  POSITIVE,
+  NEGATIVE,
+}
+
+class CustomToast extends StatelessWidget {
+  /// 토스트 메세지
+  final String text;
+
+  /// 토스트 타입
+  final ToastType type;
+
+  /// 토스트 메세지 노출 시간 (in milliseconds)
+  final int duration;
+
+  const CustomToast({
+    super.key,
+    required this.text,
+    required this.type,
+    this.duration = 3000, // Default duration set to 3 seconds
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 15),
+      decoration: BoxDecoration(
+        color: GreyColorSystem.Grey40,
+        borderRadius: BorderRadius.circular(25),
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          SvgPicture.asset(
+            type == ToastType.POSITIVE
+                ? 'assets/icon/toast_success.svg'
+                : 'assets/icon/toast_error.svg',
+            width: 16,
+            height: 16,
+          ),
+          const SizedBox(width: 10),
+          Text(
+            text,
+            style: const TextStyle(
+              fontWeight: FontWeight.w700,
+              fontSize: 12,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
