5359aa01a9238fafb3af205ee45c2d548d72d62d
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-09-11 02:45:22 +0900

    feat: not my turn screen

--- project_today/lib/main.dart
+++ project_today/lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:project_today/screen/alarm/alarm_screens.dart';
+import 'package:project_today/screen/diary/timer_screens.dart';
+import 'package:project_today/screen/diary/waiting_screens.dart';
 import 'package:project_today/screen/generate/generate_screens.dart';
 import 'package:project_today/screen/group/group_screens.dart';
 import 'package:project_today/screen/diary/diary_screens.dart';
+import 'package:project_today/screen/invite/invite_screens.dart';
 import 'package:project_today/screen/onboard/onboard_screens.dart';
+import 'package:project_today/screen/past/past_screens.dart';
+import 'package:project_today/screen/read/read_screens.dart';
 import 'package:project_today/screen/screens_index.dart';
 import 'package:project_today/screen/setting/setting_screens.dart';
 import 'package:project_today/screen/splash/splash_screens.dart';
@@ -33,6 +38,11 @@ class MyApp extends StatelessWidget {
         '/alarm': (context) => AlarmScreen(),
         '/setting': (context) => SettingScreen(),
         '/generate': (context) => GenerateScreen(),
+        '/past': (context) => PastScreen(),
+        '/read': (context) => ReadScreen(),
+        '/invite': (context) => InviteScreen(),
+        '/timer': (context) => TimerScreen(),
+        '/waiting': (context) => WaitingScreens(),
       },
       initialRoute: '/',
     );
