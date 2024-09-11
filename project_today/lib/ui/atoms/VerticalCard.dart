687b094351b6cac6740131ae9605895a1071c3f3
kweonsikyung <ekrk6632@naver.com>
2024-09-11 10:53:02 +0900

    feat: common group components

---
+++ project_today/lib/ui/atoms/VerticalCard.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:project_today/core/constant/colors.dart';
+
+class VerticalCard extends StatelessWidget {
+  /// 배경 이미지 경로
+  final String imgPath;
+
+  /// 요소 설명 텍스트
+  final String desc;
+
+  /// 버튼이 눌렸을 때 실행될 콜백 함수
+  final VoidCallback onPressed;
+
+  const VerticalCard({
+    super.key,
+    required this.imgPath,
+    required this.desc,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed,
+      child: Container(
+        width: 169.0,
+        height: 225.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14.0),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              width: 169.0,
+              height: 225.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(14.0),
+                image: DecorationImage(
+                  image: AssetImage(imgPath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 67.0,
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.63),
+                  borderRadius: const BorderRadius.vertical(
+                    bottom: Radius.circular(14.0),
+                  ),
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  desc,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.w400,
+                    color: GreyColorSystem.Grey70,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ project_today/lib/ui/organisms/gradientHeader.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:project_today/core/constant/colors.dart';
+import 'package:project_today/ui/organisms/header.dart';
+
+class GradientHeader extends StatelessWidget {
+  /// 페이지네이션을 위한 현재 페이지 인덱스
+  final int currentPage;
+
+  /// 헤더 텍스트
+  final String text;
+
+  /// 헤더 컬러 타입 (Pink | Orange) //TODO: enum으로 리팩토링
+  final String colorType;
+
+  const GradientHeader({
+    required this.currentPage,
+    required this.text,
+    required this.colorType,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = (colorType == 'Pink')
+        ? [
+            PinkColorSystem.Pink,
+            PinkColorSystem.Pink30,
+            PinkColorSystem.Pink10,
+            Colors.white
+          ]
+        : [
+            OrangeColorSystem.Orange,
+            OrangeColorSystem.Orange30,
+            OrangeColorSystem.Orange10,
+            Colors.white
+          ];
+
+    return Container(
+      height: 150,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            colors[2].withOpacity(1.0),
+            colors[2].withOpacity(0.7),
+            colors[3].withOpacity(1.0),
+          ],
+          stops: [0.0, 0.8, 1.0],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Header(
+              backgroundColor: Colors.transparent,
+              showBackButton: true,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(
+                4,
+                (index) => Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 4.0),
+                  width: 8,
+                  height: 8,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: index == currentPage ? colors[0] : colors[1],
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              text,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: 21,
+                fontWeight: FontWeight.w600,
+                color: colors[0],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
