ecbc3d9cc218e8ef2641da955a1c048d7e2a5202
kweonsikyung <ekrk6632@naver.com>
2024-09-11 15:16:52 +0900

    feat: alarm screen

--- project_today/lib/screen/alarm/alarm_screens.dart
+++ project_today/lib/screen/alarm/alarm_screens.dart
@@ -1,34 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:project_today/core/constant/colors.dart';
-import 'package:project_today/ui/organisms/header.dart';
+import 'package:project_today/ui/templates/alarmTemplate.dart';
+import 'package:project_today/ui/atoms/notiListEl.dart';
+
+const notiData = [
+  {
+    'id': 0,
+    'type': NotiType.DEFAULT,
+    'content': '시경님이 콕 찔렀어요!\n오늘의 일기를 작성해보세요',
+    'desc': 'SS507',
+    'isChecked': false,
+  },
+  {
+    'id': 1,
+    'type': NotiType.DEFAULT,
+    'content': '친구들의 오늘을 담은 새로운 일기가\n도착했어요! 얼른 확인해보세요',
+    'desc': 'SS507',
+    'isChecked': false,
+  },
+  {
+    'id': 2,
+    'type': NotiType.DEFAULT,
+    'content': '오늘은 일기장에 민진님이 들어왔어요!',
+    'desc': 'SS507',
+    'isChecked': true,
+  },
+];
 
 class AlarmScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorSystem.White,
-      body: SafeArea(
-        top: true,
-        child: Column(
-          children: [
-            Header(
-              backgroundColor: Colors.transparent,
-              title: "알림",
-              showBackButton: true,
-            ),
-            Expanded(
-                child: SingleChildScrollView(
-              padding: const EdgeInsets.all(20),
-              scrollDirection: Axis.vertical,
-              child: Column(
-                children: [
-                  Text('This is alarm screen'),
-                ],
-              ),
-            ))
-          ],
-        ),
-      ),
-    );
+    return alarmTemplate(notiData: notiData);
   }
 }
