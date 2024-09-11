02526816ed57bf77b4052933c8b64ef45b5bf3f4
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-09-11 15:36:08 +0900

    feat: past screen

--- project_today/lib/screen/past/past_screens.dart
+++ project_today/lib/screen/past/past_screens.dart
@@ -3,38 +3,11 @@ import 'package:project_today/core/constant/index.dart';
 import 'package:project_today/screen/collection/collection_screens.dart';
 import 'package:project_today/screen/member/member_screens.dart';
 import 'package:project_today/ui/organisms/index.dart';
+import 'package:project_today/ui/templates/index.dart';
 
 class PastScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: ColorSystem.White,
-      body: SafeArea(
-        child: Column(
-          children: [
-            Header(
-              showBackButton: true,
-              title: '지난 일기장',
-              showSettingsIcon: true,
-            ),
-            SizedBox(
-              height: 211,
-            ),
-            Expanded(
-              child: CustomTabComponent(
-                tabs: [
-                  Tab(text: '보관함'),
-                  Tab(text: '멤버'),
-                ],
-                tabViews: [
-                  CollectionScreen(),
-                  MemberScreen(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
+    return Tabbartemplate(headerTitle: '지난 일기장');
   }
 }
