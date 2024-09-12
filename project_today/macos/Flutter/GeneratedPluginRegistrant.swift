b065b0a7b18183bbea7b1b09baa263bf404e5c6e
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-09-13 00:28:34 +0900

    fix: resolve dependency issues

--- project_today/macos/Flutter/GeneratedPluginRegistrant.swift
+++ project_today/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,14 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import emoji_picker_flutter
+import file_selector_macos
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  EmojiPickerFlutterPlugin.register(with: registry.registrar(forPlugin: "EmojiPickerFlutterPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
