a69c3b9774f4be65b1a01e7ffae8016d5bdac8f0
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-09-12 23:02:53 +0900

    add: write screen

---
+++ project_today/lib/screen/write/write_screens.dart
@@ -0,0 +1,45 @@
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:pro_image_editor/models/editor_callbacks/pro_image_editor_callbacks.dart';
+import 'package:pro_image_editor/modules/main_editor/main_editor.dart';
+import 'package:project_today/screen/write/check_screens.dart';
+
+class WriteScreen extends StatefulWidget {
+  @override
+  _WriteScreenState createState() => _WriteScreenState();
+}
+
+class _WriteScreenState extends State<WriteScreen> {
+  Uint8List? editedBytes;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ProImageEditor.network(
+        'https://i.pinimg.com/564x/e4/1f/dc/e41fdc4bd72fa4689ab5710f9b13f0e7.jpg',
+        callbacks: ProImageEditorCallbacks(
+          onImageEditingComplete: (Uint8List bytes) async {
+            print('Image editing completed.');
+            print('Image size in bytes: ${bytes.length}');
+
+            editedBytes = bytes;
+          },
+          onCloseEditor: () {
+            if (editedBytes != null) {
+              // 이미지가 null이 아닐 때 다음 화면으로 이동
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => CheckScreen(imageData: editedBytes!),
+                ),
+              );
+            } else {
+              // 이미지가 null일 경우 현재 화면을 닫음
+              Navigator.of(context).pop();
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
