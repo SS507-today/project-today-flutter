95d77fa20e9172598531683fa9e2432c5957b7da
kweonsikyung <ekrk6632@naver.com>
2024-09-25 15:41:14 +0900

    refactor: 설정스크린 내 템플릿 분리

---
+++ project_today/lib/ui/templates/settingTemplate.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'dart:io'; // File을 다루기 위해 필요
+import 'package:flutter_svg/svg.dart';
+import 'package:project_today/ui/organisms/header.dart';
+import 'package:project_today/ui/atoms/atoms.dart';
+import 'package:image_picker/image_picker.dart';
+
+class SettingTemplate extends StatelessWidget {
+  final XFile? image;
+  final VoidCallback onImagePick;
+  final VoidCallback onChangeNickname;
+  final VoidCallback onLogout;
+  final String nickname;
+
+  const SettingTemplate({
+    required this.image,
+    required this.onImagePick,
+    required this.onChangeNickname,
+    required this.onLogout,
+    required this.nickname,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        top: true,
+        child: Column(
+          children: [
+            Header(
+              backgroundColor: Colors.transparent,
+              title: "설정",
+              showBackButton: true,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 23, bottom: 33),
+                    child: Stack(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          backgroundImage: image != null
+                              ? FileImage(File(image!.path))
+                              : AssetImage('assets/images/i1.png')
+                                  as ImageProvider,
+                          backgroundColor: Colors.grey[300],
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: GestureDetector(
+                            onTap: onImagePick,
+                            child: CircleAvatar(
+                              backgroundColor: Colors.white,
+                              child: SvgPicture.asset(
+                                'assets/icons/ic_cam.svg',
+                                width: 34.0,
+                                height: 34.0,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  DefaultListEl(
+                    title: '닉네임 변경',
+                    desc: nickname,
+                    onPressed: onChangeNickname,
+                    isShowArrow: true,
+                  ),
+                  SizedBox(height: 14),
+                  DefaultListEl(
+                    title: '로그아웃',
+                    onPressed: onLogout,
+                    isShowArrow: false,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
