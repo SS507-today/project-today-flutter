345dd59af6efc71ef65cf336920350cea1e98195
kweonsikyung <ekrk6632@naver.com>
2024-10-13 21:49:01 +0900

    add: `/participate` `/invite` 라우터 추가

--- project_today/lib/main.dart
+++ project_today/lib/main.dart
@@ -7,7 +7,7 @@ import 'package:project_today/screen/home/diary_screens.dart';
 import 'package:project_today/screen/generate/view/generate_view.dart';
 import 'package:project_today/screen/group/view/group_view.dart';
 import 'package:project_today/screen/home/view/home_view.dart';
-import 'package:project_today/screen/invite/invite_screens.dart';
+import 'package:project_today/screen/invite/view/invite_view.dart';
 import 'package:project_today/screen/onboard/onboard_screens.dart';
 import 'package:project_today/screen/prev/prev_screens.dart';
 import 'package:project_today/screen/read/read_screens.dart';
@@ -16,6 +16,7 @@ import 'package:project_today/screen/login/login_screens.dart';
 import 'package:project_today/screen/write/write_screens.dart';
 import 'package:project_today/screen/change/change_screens.dart';
 import 'package:project_today/screen/member/view/member_view.dart';
+import 'package:project_today/screen/participate/view/participate_view.dart';
 
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
@@ -46,8 +47,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       routes: {
-        '/': (context) => SplashScreen(), //회원가입 여부 조회 후 분기 뷰
-        '/login': (context) => LoginScreen(), //카카오 로그인 뷰
+        '/': (context) => SplashScreen(),
+        '/login': (context) => LoginScreen(),
         '/group': (context) => GroupView(), //내 그룹 리스트 렌더 뷰
         '/onboard': (context) => OnboardScreen(),
         '/alarm': (context) => AlarmScreen(),
@@ -56,12 +57,13 @@ class MyApp extends StatelessWidget {
         '/generate': (context) => GenerateView(), //그룹 생성 뷰
         '/prev': (context) => PrevScreen(),
         '/read': (context) => ReadScreen(),
-        '/invite': (context) => InviteScreen(),
         '/timer': (context) => TimerView(), //교환일기 시작 전 타이머 뷰
         '/waiting': (context) => WaitingView(), //교환일기 내 차례 아닐 때 뷰
         '/diary': (context) => DiaryView(), //교환일기 내 차례일 때 뷰
         '/write': (context) => WriteScreen(),
         '/member': (context) => MemberScreen(),
+        '/participate': (context) => ParticipateView(), //참가 코드 입력 뷰
+        '/invite': (context) => InviteView(), //유효한 참가 코드 시 그룹 정보 렌더 뷰
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/home') {
