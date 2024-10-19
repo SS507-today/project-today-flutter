8ae767ff7cd902981be7debd966d7a5b6307438b
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-10-20 03:54:16 +0900

    feat: Add CustomToastManager initialization in SplashView

--- project_today/lib/screen/splash/view/splash_view.dart
+++ project_today/lib/screen/splash/view/splash_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project_today/screen/splash/view_model/splash_view_model.dart';
 import 'package:project_today/ui/templates/index.dart';
+import 'package:project_today/ui/atoms/index.dart';
 
 class SplashView extends StatelessWidget {
   final SplashViewModel _splashViewModel = Get.put(SplashViewModel());
@@ -9,6 +10,7 @@ class SplashView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     _splashViewModel.checkUserStatusAfterDelay(context);
+    CustomToastManager().init(context);
 
     return Introtemplate(
       showLoginButton: false,
