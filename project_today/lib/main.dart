d12e7286981b837459192d96c67417cc2ce92fe3
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-10-20 03:37:10 +0900

    fix: 불필요 파일 제거

--- project_today/lib/main.dart
+++ project_today/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project_today/screen/alarm/alarm_screens.dart';
+import 'package:project_today/screen/group_setting/view/group_setting_view.dart';
 import 'package:project_today/screen/home/timer_screens.dart';
 import 'package:project_today/screen/home/waiting_screens.dart';
 import 'package:project_today/screen/home/diary_screens.dart';
@@ -7,13 +7,13 @@ import 'package:project_today/screen/generate/view/generate_view.dart';
 import 'package:project_today/screen/group/view/group_view.dart';
 import 'package:project_today/screen/home/view/home_view.dart';
 import 'package:project_today/screen/invite/view/invite_view.dart';
-import 'package:project_today/screen/prev/prev_screens.dart';
-import 'package:project_today/screen/read/read_screens.dart';
+import 'package:project_today/screen/prev/view/prev_view.dart';
 import 'package:project_today/screen/onboard/view/onboard_view.dart';
+import 'package:project_today/screen/read/view/read_view.dart';
 import 'package:project_today/screen/setting/view/setting_view.dart';
 import 'package:project_today/screen/splash/view/splash_view.dart';
 import 'package:project_today/screen/login/view/login_view.dart';
-import 'package:project_today/screen/write/write_screens.dart';
+import 'package:project_today/screen/write/view/write_view.dart';
 import 'package:project_today/screen/change/view/change_view.dart';
 import 'package:project_today/screen/member/view/member_view.dart';
 import 'package:project_today/screen/participate/view/participate_view.dart';
@@ -43,6 +43,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         fontFamily: "Pretendard",
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Colors.white, // 기본 배경 색 흰색으로 설정
+        progressIndicatorTheme: ProgressIndicatorThemeData(
+          color: Colors.pink, // 로딩 인디케이터 색 핑크로 설정
+        ),
         useMaterial3: true,
       ),
       routes: {
@@ -50,19 +54,19 @@ class MyApp extends StatelessWidget {
         '/login': (context) => LoginView(),
         '/group': (context) => GroupView(), //내 그룹 리스트 렌더 뷰
         '/onboard': (context) => OnboardView(),
-        '/alarm': (context) => AlarmScreen(),
         '/setting': (context) => SettingView(),
         '/change': (context) => ChangeView(),
         '/generate': (context) => GenerateView(), //그룹 생성 뷰
-        '/prev': (context) => PrevScreen(),
-        '/read': (context) => ReadScreen(),
+        '/prev': (context) => PrevView(),
+        '/read': (context) => ReadView(),
         '/timer': (context) => TimerView(), //교환일기 시작 전 타이머 뷰
         '/waiting': (context) => WaitingView(), //교환일기 내 차례 아닐 때 뷰
         '/diary': (context) => DiaryView(), //교환일기 내 차례일 때 뷰
-        '/write': (context) => WriteScreen(),
-        '/member': (context) => MemberScreen(),
+        '/write': (context) => WriteView(),
+        '/member': (context) => MemberView(),
         '/participate': (context) => ParticipateView(), //참가 코드 입력 뷰
         '/invite': (context) => InviteView(), //유효한 참가 코드 시 그룹 정보 렌더 뷰
+        '/groupsetting': (context) => GroupSettingView(),
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/home') {
