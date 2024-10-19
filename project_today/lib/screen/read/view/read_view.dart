a8540da95aa9b1c277c23cd9ca777e3244c109fb
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-10-20 03:32:01 +0900

    fix: readView isWeek에 따른 버튼 활성화 여부 추가

--- project_today/lib/screen/read/view/read_view.dart
+++ project_today/lib/screen/read/view/read_view.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project_today/core/constant/colors.dart';
+import 'package:project_today/ui/atoms/atoms.dart';
 import 'package:project_today/ui/templates/index.dart';
+import 'package:project_today/ui/organisms/index.dart';
 import 'package:project_today/screen/read/view_model/read_view_model.dart';
 import 'package:project_today/data/repositories/current_group_id_repository.dart'
     as globals;
 
 class ReadView extends StatefulWidget {
-  final bool isWeek;
-
-  const ReadView({
-    Key? key,
-    this.isWeek = false,
-  }) : super(key: key); // 생성자에 추가
+  const ReadView({Key? key}) : super(key: key);
 
   @override
   _ReadViewState createState() => _ReadViewState();
@@ -28,7 +25,7 @@ class _ReadViewState extends State<ReadView> {
 
     // initState에서 context를 사용하면 안되므로 didChangeDependencies에서 처리
     if (!_isInitialized) {
-// arguments를 Map으로 받음
+      // arguments를 Map으로 받음
       final args =
           ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
 
@@ -43,7 +40,9 @@ class _ReadViewState extends State<ReadView> {
           print("번들 ID가 없습니다.");
         }
 
-        _isInitialized = true;
+        setState(() {
+          _isInitialized = true;
+        });
       } else {
         print("전달된 인수가 없습니다.");
       }
@@ -52,6 +51,11 @@ class _ReadViewState extends State<ReadView> {
 
   @override
   Widget build(BuildContext context) {
+    // isWeek 값을 ModalRoute에서 받아오기
+    final args =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>?;
+    final bool isWeek = args?['isWeek'] as bool? ?? false; // 전달받은 isWeek 사용
+
     return Scaffold(
       backgroundColor: ColorSystem.White,
       body: SafeArea(
@@ -62,9 +66,27 @@ class _ReadViewState extends State<ReadView> {
           }
 
           final diaries = _readViewModel.diaries;
-
+          //TODO : 서버에 요청해서 isWrited 받아오기
+          // 일기 데이터가 없을 때
           if (diaries.isEmpty) {
-            return const Center(child: Text('다이어리 데이터가 없습니다.'));
+            return Column(
+              children: [
+                Header(showBackButton: true),
+                Expanded(
+                  child: Center(
+                    child: Text('아직 다이어리 데이터가 없어요!'),
+                  ),
+                ),
+                if (isWeek) // isWeek가 true일 때만 버튼 표시
+                  DefaultActionButton(
+                    text: '일기 쓰기',
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/write');
+                    },
+                    isActive: true,
+                  ),
+              ],
+            );
           }
 
           // 서버에서 불러온 데이터로 이미지와 날짜 및 사용자 정보 리스트 생성
@@ -83,7 +105,7 @@ class _ReadViewState extends State<ReadView> {
             dates: dates,
             imageUrls: imageUrls,
             userInfo: userInfo,
-            showButton: widget.isWeek, // isWeek를 showButton에 전달
+            showButton: isWeek, // ModalRoute에서 받은 isWeek를 showButton에 전달
             onWriteButtonPressed: () {
               Navigator.pushNamed(context, '/write');
             },
