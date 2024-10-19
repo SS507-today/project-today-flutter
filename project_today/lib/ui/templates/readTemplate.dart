0985cf87426281b06f512cf16b4a72ea75c44abe
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-10-20 02:37:46 +0900

    feat: ReadTemplates

---
+++ project_today/lib/ui/templates/readTemplate.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:project_today/ui/atoms/atoms.dart';
+import 'package:project_today/ui/organisms/index.dart';
+
+class ReadTemplates extends StatelessWidget {
+  final List<String> dates;
+  final List<String> imageUrls;
+  final List<Map<String, String>> userInfo;
+  final VoidCallback? onWriteButtonPressed;
+  final bool showButton;
+
+  const ReadTemplates({
+    super.key,
+    required this.dates,
+    required this.imageUrls,
+    required this.userInfo,
+    this.onWriteButtonPressed,
+    this.showButton = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Header(showBackButton: true),
+        Expanded(
+          child: SingleChildScrollView(
+            child: ImageCarouselWithProfile(
+              dates: dates,
+              imageUrls: imageUrls,
+              userInfo: userInfo,
+            ),
+          ),
+        ),
+        if (showButton)
+          DefaultActionButton(
+            text: '일기 쓰기',
+            onPressed: onWriteButtonPressed ?? () {},
+            isActive: true,
+          ),
+      ],
+    );
+  }
+}
