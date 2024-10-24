1e2d285a66f9f62489dc8372ace4f9a32a73094b
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-10-25 01:20:31 +0900

    refactor: SettingView 로딩 스켈레톤 UI 추가

--- project_today/lib/screen/setting/view/setting_view.dart
+++ project_today/lib/screen/setting/view/setting_view.dart
@@ -2,33 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:project_today/screen/setting/view_model/setting_view_model.dart';
 import 'package:project_today/ui/templates/settingTemplate.dart';
+import 'package:skeletonizer/skeletonizer.dart'; // 스켈레톤 로딩 추가
 
 class SettingView extends StatelessWidget {
-  final SettingViewModel _userController = Get.put(SettingViewModel());
+  final SettingViewModel _viewModel = Get.put(SettingViewModel());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Obx(() {
-        final user = _userController.user.value;
+        final user = _viewModel.user.value;
+        final isLoading = _viewModel.isLoading.value;
 
-        return SettingTemplate(
-          image: user.image,
-          nickname: user.nickname,
-          onChangeNickname: () async {
-            // 닉네임 변경 화면으로 이동
-            await Navigator.pushNamed(context, '/change');
-            // 닉네임이 변경된 후 사용자 정보 갱신
-            _userController.fetchUserInfo();
-          },
-          isGroupSetting: false,
-          onLogout: _onLogout,
+        return Skeletonizer(
+          enabled: isLoading, // 로딩 중일 때 Skeletonizer 활성화
+          child: SettingTemplate(
+            image: user.image,
+            nickname: user.nickname,
+            onChangeNickname: () async {
+              // 닉네임 변경 화면으로 이동
+              await Navigator.pushNamed(context, '/change');
+              // 닉네임이 변경된 후 사용자 정보 갱신
+              _viewModel.fetchUserInfo();
+            },
+            isGroupSetting: false,
+            onLogout: _onLogout,
+          ),
         );
       }),
     );
   }
 
-//TODO 진짜 로그인 함수
+  // 로그아웃 처리
   void _onLogout() {
     Get.dialog(
       AlertDialog(
