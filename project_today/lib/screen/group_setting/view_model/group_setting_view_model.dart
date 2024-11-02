11bbd77cc16b0d35e4fcdbbb04b9c82c4fdccc9d
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-11-03 00:04:18 +0900

    feat: 그룹 탈퇴

--- project_today/lib/screen/group_setting/view_model/group_setting_view_model.dart
+++ project_today/lib/screen/group_setting/view_model/group_setting_view_model.dart
@@ -1,14 +1,20 @@
+import 'dart:ffi';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import 'package:project_today/data/repositories/auth_repository.dart';
 import 'package:project_today/data/repositories/group_profile_repository.dart';
 import 'package:project_today/data/models/index.dart';
 import 'package:project_today/data/repositories/current_group_id_repository.dart'
     as globals;
+import 'package:project_today/data/services/delete_share_group.dart';
+import 'package:project_today/screen/group/view/group_view.dart';
+import 'package:project_today/ui/atoms/index.dart';
 
 class GroupSettingViewModel extends GetxController {
   final AuthRepository _authRepository = AuthRepository();
   final GroupProfileRepository _groupProfileRepository =
       GroupProfileRepository();
+  final DeleteShareGroup _deleteShareGroup = DeleteShareGroup();
 
   /// 그룹 프로필 정보를 관리하는 상태 변수
   var groupProfile = GroupProfile(
@@ -89,6 +95,38 @@ class GroupSettingViewModel extends GetxController {
         profile.description = newDescription;
       }
     });
+  }
+
+  /// 그룹 탈퇴
+
+  Future<void> leaveGroup(BuildContext context) async {
+    final tokens = await _authRepository.loadTokens();
+    final String? accessToken = tokens['jwtAccessToken'];
+    // 그룹 탈퇴 API 호출
+    final result = await _deleteShareGroup.leaveShareGroup(
+        globals.currentGroupId.value, accessToken!);
+    // API 호출 결과 처리
+    if (result != null) {
+      print("그룹 탈퇴 성공: ${result['shareGroupId']}번 그룹에서 탈퇴했습니다.");
+
+      CustomToastManager().showCustomToast(
+        message: "그룹에서 탈퇴했습니다.",
+        type: ToastType.POSITIVE,
+      );
+      // 쌓여 있는 모든 라우트를 제거하고 로그인 화면으로 이동
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(
+            builder: (context) => GroupView()), // LoginView를 직접 지정
+        (Route<dynamic> route) => false,
+      );
+    } else {
+      CustomToastManager().showCustomToast(
+        message: "그룹 탈퇴 실패",
+        type: ToastType.NEGATIVE,
+      );
+      print("그룹 탈퇴 실패");
+    }
   }
 
   /// 그룹 프로필 정보 초기화 (로그아웃 또는 그룹 변경 시 사용)
