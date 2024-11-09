864be9c0abaf8244025daffa036259f2d3eccd87
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-11-10 03:09:02 +0900

    fix: collection view 버그 해결

--- project_today/lib/screen/collection/view/collection_view.dart
+++ project_today/lib/screen/collection/view/collection_view.dart
@@ -26,30 +26,30 @@ class _CollectionViewState extends State<CollectionView> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Obx(() {
-        final bundles = _collectionViewModel.bundlesList;
-        /*.length > 1
-            ? _collectionViewModel.bundlesList.sublist(1) // 첫 번째 번들 제외
-            : [];
-*/
-        final bundleData = bundles.map((bundle) {
-          return {
-            'id': bundle.bundleId,
-            'imgsrc': _collectionViewModel.coverImage != 0
-                ? 'assets/images/cover/group_${_collectionViewModel.coverImage}.png'
-                : 'assets/images/temp_bg.png', // 커버 이미지를 번들에 맞게 설정
-            'desc': '${bundle.startDate} ~ ${bundle.endDate}',
-          };
-        }).toList();
+    return Obx(() {
+      final bundles = _collectionViewModel.bundlesList;
+      final bundleData = bundles.map((bundle) {
+        return {
+          'id': bundle.bundleId,
+          'imgsrc': _collectionViewModel.coverImage != 0
+              ? 'assets/images/cover/group_${_collectionViewModel.coverImage}.png'
+              : 'assets/images/temp_bg.png',
+          'desc': '${bundle.startDate} ~ ${bundle.endDate}',
+        };
+      }).toList();
 
-        return bundles.isEmpty
-            ? Center(child: Text('번들이 없습니다.')) // 번들이 없을 때 대체 메시지
-            : ExpandedGridView(
-                groupData: bundleData,
-                isLoading: isLoading,
-              );
-      }),
-    );
+      return Scaffold(
+        body: bundles.isEmpty
+            ? Center(child: Text('번들이 없습니다.'))
+            : Column(
+                children: [
+                  ExpandedGridView(
+                    groupData: bundleData,
+                    isLoading: isLoading,
+                  ),
+                ],
+              ),
+      );
+    });
   }
 }
--- project_today/lib/ui/organisms/expanded_grid_view.dart
+++ project_today/lib/ui/organisms/expanded_grid_view.dart
@@ -12,35 +12,35 @@ class ExpandedGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: GridView.builder(
-        padding: const EdgeInsets.all(20), // 좌우 패딩 추가
-        itemCount: groupData.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-          childAspectRatio: 0.75, // 카드 비율 설정
-        ),
-        itemBuilder: (context, index) {
-          final group = groupData[index];
-          return VerticalCard(
-            imgPath: group['imgsrc'] as String,
-            desc: group['desc'] as String,
-            onPressed: () {
-              Navigator.pushNamed(
-                context,
-                '/read',
-                arguments: {
-                  'bundleId': group['id'],
-                  'isWeek': false,
-                },
-              );
-            },
-            isLoading: isLoading,
-          );
-        },
+    return GridView.builder(
+      padding: const EdgeInsets.all(20),
+      shrinkWrap: true, // GridView가 `Column` 내부에서 작동하도록 함
+      physics: const NeverScrollableScrollPhysics(), // 스크롤 비활성화
+      itemCount: groupData.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+        childAspectRatio: 0.75,
       ),
+      itemBuilder: (context, index) {
+        final group = groupData[index];
+        return VerticalCard(
+          imgPath: group['imgsrc'] as String,
+          desc: group['desc'] as String,
+          onPressed: () {
+            Navigator.pushNamed(
+              context,
+              '/read',
+              arguments: {
+                'bundleId': group['id'],
+                'isWeek': false,
+              },
+            );
+          },
+          isLoading: isLoading,
+        );
+      },
     );
   }
 }
