333bb744d52e54db2619793fee96d386e11cfcf1
bakhwee-bug <95680133+bakhwee-bug@users.noreply.github.com>
2024-11-10 02:43:46 +0900

    refactor: 카카오로그인 버튼 반응형으로 수정

--- project_today/lib/ui/atoms/kakaoLoginButton.dart
+++ project_today/lib/ui/atoms/kakaoLoginButton.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class KakaoLoginButton extends StatelessWidget {
   /// 카카오 로그인 실행 콜백 함수
@@ -15,16 +16,34 @@ class KakaoLoginButton extends StatelessWidget {
       margin: const EdgeInsets.symmetric(vertical: 14, horizontal: 20),
       width: double.infinity,
       height: 53,
-      child: GestureDetector(
-        onTap: onPressed,
-        child: DecoratedBox(
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage('assets/images/kakao.png'),
-              fit: BoxFit.cover,
-            ),
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color(0xFFFEE500), // 배경색 설정
+          shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(14),
           ),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          elevation: 0,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SvgPicture.asset(
+              'assets/icons/ic_kakao_login.svg',
+              width: 24,
+              height: 24,
+            ),
+            const SizedBox(width: 10),
+            const Text(
+              '카카오 로그인',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+          ],
         ),
       ),
     );
